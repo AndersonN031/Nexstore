2f4133a92f81105b6d368a913d493743a769858a
Anderson Nunes <devandersonnunes078@gmail.com>
2024-02-01 11:15:12 -0300

    adicionando overlay na pag products

--- app/components/StoreProducts.tsx
+++ app/components/StoreProducts.tsx
@@ -1,7 +1,7 @@
-import React from "react"
-import UseProduct from "../hooks/useProducts"
-import Link from "next/link"
-import Button from "./Button"
+import { useState } from 'react';
+import UseProduct from '../hooks/useProducts';
+import Link from 'next/link';
+import Button from './Button';
 
 
 export default function Products() {
@@ -11,33 +11,37 @@ export default function Products() {
         return price.toLocaleString('pt-BR', {
             style: 'currency',
             currency: 'BRL'
-        })
+        });
     }
 
     return (
         <div className="container-clothes">
-            {productList.map((product) => (
-                <div key={product.id} className="flex">
-                    <div className="card-content">
-
-                        <div className="image-container">
-                            <Link
-                                href={`/products/${product.id}`}
-                                passHref
-                            >
-                                <img src={product.image} alt={product.title} />
-                            </Link>
+            {productList.map((product: any) => (
+                <div
+                    key={product.id}
+                    className="flex"
+                >
+                    <div className='card-container'>
+                        <div className="card-content">
+                            <div className="image-container">
+                                <Link
+                                    href={`/products/${product.id}`}
+                                    passHref
+                                >
+                                    <img src={product.image} alt={product.title} />
+                                    <div className='overlay'>
+                                        <p>Ver produto</p>
+                                    </div>
+                                </Link>
+                            </div>
+                            <p className="text-title">{product.title}</p>
+                            <p className="text-price">{formatedPrice(product.price)}</p>
+                            <Button title="Adicionar ao carrinho" />
                         </div>
-
-                        <p className="text-title">{product.title}</p>
-                        <p>{formatedPrice(product.price)}</p>
-                        <Button title="Adicionar ao carrinho" />
-                        {/* <Button title="Ver detalhes" /> */}
-
-
                     </div>
                 </div>
             ))}
         </div>
     )
 }
+
