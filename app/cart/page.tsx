b7fe538a5b476b15caf0901f9256f25506697f7b
Anderson Nunes <devandersonnunes078@gmail.com>
2024-02-14 11:05:49 -0300

    criando uma tela para pagina cart

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -41,46 +41,45 @@ export default function Cart() {
             console.error('Produto não encontrado no carrinho.')
         }
         // Exibe no console o cartItem (pode ser nulo se o produto não foi encontrado)
-
-
-        return (
-            <>
-                <Header />
-                <Sidebar />
-                <div className="main-cart">
-                    <ul className="main-table">
-                        <div className="icon-container">
-                            <i className="bi bi-cart-fill"></i>
-                            <h1>Meu carrinho</h1>
-                        </div>
-                        <table className="ecommerce-table">
-                            <thead>
-                                <tr>
-                                    <th>Produto</th>
-                                    <th>Preço</th>
-                                    <th>Qtd.</th>
-                                    <th>Total</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {cart.map((cartItem) => (
-                                    <tr key={cartItem.product.id}>
-
-                                        <td className="td-img-title"><img src={cartItem.product.image} alt="" /> {cartItem.product.title} </td>
-                                        <td>{formatedPrice(cartItem.product.price)}</td>
-                                        <td className="cart-quantity">{cartItem.quantity}</td>
-                                        <td>{formatedPrice(cartItem.quantity * cartItem.product.price)}</td>
-                                        <td>
-                                            <button onClick={() => handleAddProduct(cartItem.product.id)}>+</button>
-                                            <button onClick={() => handleRemoveProduct(cartItem.product.id)}>-</button>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </ul>
-                </div>
-            </>
-        )
     }
+
+    return (
+        <>
+            <Header />
+            <Sidebar />
+            <div className="main-cart">
+                <ul className="main-table">
+                    <div className="icon-container">
+                        <i className="bi bi-cart-fill"></i>
+                        <h1>Meu carrinho</h1>
+                    </div>
+                    <table className="ecommerce-table">
+                        <thead>
+                            <tr>
+                                <th>Produto</th>
+                                <th>Preço</th>
+                                <th>Qtd.</th>
+                                <th>Total</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {cart.map((cartItem) => (
+                                <tr key={cartItem.product.id}>
+
+                                    <td className="td-img-title"><img src={cartItem.product.image} alt="" /> {cartItem.product.title} </td>
+                                    <td>{formatedPrice(cartItem.product.price)}</td>
+                                    <td className="cart-quantity">{cartItem.quantity}</td>
+                                    <td>{formatedPrice(cartItem.quantity * cartItem.product.price)}</td>
+                                    <td>
+                                        <button onClick={() => handleAddProduct(cartItem.product.id)}>+</button>
+                                        <button onClick={() => handleRemoveProduct(cartItem.product.id)}>-</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </ul>
+            </div>
+        </>
+    )
 }
