535db32f9cdafc56c5e62702fb3528c8e0f5d632
Anderson Nunes <devandersonnunes078@gmail.com>
2024-02-20 17:16:01 -0300

    addng search

--- app/components/HeaderComponent.tsx
+++ app/components/HeaderComponent.tsx
@@ -16,28 +16,43 @@ export default function Header() {
     const [products, setProducts] = useState<ProductTypes[]>([])
     const [filteredProducts, setFilteredProducts] = useState<ProductTypes[]>([]);
     const [selectOptions, setSelectOptions] = useState<{ label: string, value: string }[]>([])
+
     // atualizando os produtos quando o productList for alterado
     useEffect(() => {
+        // verificando se o productList existe, se sim, ele atualizará os estuados abaixo...
         if (productList) {
             setProducts(productList)
+
+            // filtro para procurar o produto de acordo com o que está digitado no input
             setSelectOptions(productList.map(product => ({ label: product.title, value: product.id.toString() })))
         }
     }, [productList])
 
 
+    // é acionada quando há uma mudança no no valor selecionado no component <Select>
+
+    /* selectedOption: É o objeto representando a opção selecionada no componente Select. Possui propriedades como label e value.
+    actionMeta: É um objeto que fornece informações adicionais sobre a ação que causou a mudança. */
     const handleSearchChange = (selectedOption: SingleValue<{ label: string; value: string }>, actionMeta: ActionMeta<{ label: string; value: string }>) => {
+
+        // verificando se o selectedOption é verdadeiro...
         if (selectedOption) {
             setSearch(selectedOption.label);
         } else {
-            setSearch('');
+            // setSearch('');
         }
     }
 
 
+    // função de submit 
     const handleSearchSubmit = (event: React.FormEvent) => {
         event.preventDefault();
+
+        // remove os espaços em branco extras no início e no final da string de pesquisa...
         const trimmedSearch = search.trim()
 
+        // verifica se existe alguma pesquisa após remoção de espaços em branco
+        /*ADICIONAR UMA ROTA CASO O PRODUTO NÃO SEJÁ ENCONTRADO CASO O USUÁRIO DIGITE ALGO ERRADO*/
         if (trimmedSearch.length > 0) {
 
             const newFilteredProducts = products.filter(product => product.title.toLowerCase().includes(search.toLowerCase()));
